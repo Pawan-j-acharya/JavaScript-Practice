4d520dfa3e898fb0e102e1a800c1b7ae274f994d
Pawan-j-acharya <pawanachar20@gmail.com>
2023-11-01 13:49:33 +0530

    fourth commit

---
+++ beginner_javascript/test.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+
+// console.log(document.getElementById("topbar"));
+// console.log(document.getElementById("logo"));
+
+// console.log(document.getElementsByClassName('blog-card'));
+
+console.log(document.getElementsByTagName('img'));
